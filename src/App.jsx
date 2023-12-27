a2cbb39abb005523946e0bca627d6dd82f3b764b
abh-i-navv <abhinav11290@gmail.com>
2023-12-27 14:18:18 +0530

    used rough js

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import { FaPen } from "react-icons/fa";
 import { MdOutlineRectangle } from "react-icons/md";
 import { MdOutlineClear } from "react-icons/md";
 import { Clear } from './scripts/Clear';
+import { IoRemoveOutline } from "react-icons/io5";
 
 function App() {
   
-  const [currentTool, setCurrentTool] = useState('pen')
+  const [currentTool, setCurrentTool] = useState('line')
   const canvasRef = useRef(null)
 
   return (
     <>
       <div>
       <div className='flex flex-row justify-center items-center'>
-      <div className='border-black border-2 cursor-pointer m-2 p-2'>
+      
+      {/* <div className='border-black border-2 cursor-pointer m-2 p-2'>
         <FaPen onClick={()=>{setCurrentTool('pen')}} />
+      </div> */}
+      <div className='border-black border-2 cursor-pointer m-2 p-2'>
+        <IoRemoveOutline onClick={()=>{setCurrentTool('line')}} />
       </div>
       <div className='border-black border-2 cursor-pointer m-2 p-2'>
         <MdOutlineRectangle onClick={()=>{setCurrentTool('rectangle')}} />
--- src/components/DrawingArea.jsx
+++ src/components/DrawingArea.jsx
@@ -1,4 +1,10 @@
 import React, { useState,useRef, useEffect } from 'react'
+import rough from 'roughjs';
+
+const gen = rough.generator()
+
+
+
 
 function DrawingArea(props) {
 
@@ -6,54 +12,74 @@ function DrawingArea(props) {
     const {currentTool} = props
     const [isDrawing, setDrawing] = useState(false)
     const [prevPos,setPrevPos] = useState({x:0,y:0})
+    const [elements, setElements] = useState([])
+
+    const createElement = (x1,y1,x2,y2,ctx) => {
+
+      if(currentTool === 'line'){
+        const element = gen.line(x1,y1,x2,y2)
+        return {x1,y1,x2,y2, element}
+      }
+
+      if(currentTool === 'rectangle'){
+        const element = gen.rectangle(x1,y1,x2-x1,y2-y1)
+        return {x1,y1,x2,y2, element}
+      }
+    
+    }
 
     useEffect(()=>{
       const canvas = canvasRef.current
       const ctx = canvas.getContext('2d')
+      ctx.clearRect(0,0,canvas.width,canvas.height)      
+
+      const roughCanvas =  rough.canvas(canvas)
+
+      elements.forEach(({element}) => {roughCanvas.draw(element)})
 
       const onMouseDown = (e) =>{
         setDrawing(true)
         const x = e.clientX - canvas.offsetLeft
         const y = e.clientY - canvas.offsetTop
-        setPrevPos({x,y})
+        
+        if(currentTool === 'pen'){            
+          setPrevPos({x,y})
+          ctx.beginPath()
+          ctx.moveTo(x,y)
+          
+        }
 
-        ctx.beginPath()
-        ctx.moveTo(x,y)
+        const element = createElement(x,y,x,y)
+        setElements(prev => [...prev,element])
         
       }
-
 
       const onMouseMove = (e) => {
           const x = e.clientX - canvas.offsetLeft
           const y = e.clientY - canvas.offsetTop
 
           if(!isDrawing)  return
-          
+
           if(currentTool === 'pen'){            
-              ctx.lineTo(x,y)
-              ctx.stroke()
-              setPrevPos({x,y})
-            }
-            
-            if(currentTool === 'rectangle'){
-              
-              const prevShape = {
-                x: prevPos.x,
-                y: prevPos.y,
-                width: x-prevPos.x,
-                height: y-prevPos.y
-              }
-            ctx.clearRect(prevShape.x, prevShape.y, prevShape.width, prevShape.height)
-            ctx.strokeRect(prevPos.x,prevPos.y, x-prevPos.x, y-prevPos.y)
+            ctx.lineTo(x,y)
             ctx.stroke()
+            setPrevPos({x,y})
           }
+          
+          const index = elements.length -1
+          const {x1,y1} = elements[index]
+
+          const element = createElement(x1,y1,x,y)
+
+          const temp = [...elements]
+          temp[index] = element
+          setElements(temp)
 
       }
 
       const onMouseUp = () => {
         ctx.closePath()
           setDrawing(false)
-          setPrevPos({})
           console.log(prevPos)
       }
 
@@ -67,7 +93,7 @@ function DrawingArea(props) {
         document.removeEventListener('mouseup', onMouseUp)
       }
 
-    },[isDrawing])
+    },[elements])
 
   return (
     <div className='flex justify-center'>
